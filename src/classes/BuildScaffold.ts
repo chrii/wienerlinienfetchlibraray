708516cddb818903d71ffb4155114e13c9fb9cd3
chris <c.pintarich@gmail.com>
2020-02-18 20:56:30 +0100

    dlfkgjsdfg

--- src/classes/BuildScaffold.ts
+++ src/classes/BuildScaffold.ts
@@ -1,1 +1,8 @@
-export class BuildScaffold {}
+interface IStationObject {}
+
+export class BuildScaffold {
+  constructor(public haltestellen: any, public linien: any, steige: any) {}
+  scaffold(): any {
+    return this.haltestellen;
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import haltestellen from "./ogd-data/haltestellen.json";
+import steige from "./ogd-data/steige.json";
+import linien from "./ogd-data/linien.json";
+import { BuildScaffold } from "./classes/BuildScaffold";
+
+console.log(steige);
+console.log(linien);
